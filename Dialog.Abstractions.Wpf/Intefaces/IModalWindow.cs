0fb38d8026701b9aae0a8a66f34c469e8b06b8f7
David Bruck <david.bruck@citrix.com>
2012-05-23 10:49:41 -0400

    Checking in required 3rdParty binaries along with changes to the projects so they are referenced at the new location, moved the Show method modification in the 3rdParty ChildWindow class to a new extension class outside the WPFToolkit library

--- Dialog.Abstractions.Wpf/Intefaces/IModalWindow.cs
+++ Dialog.Abstractions.Wpf/Intefaces/IModalWindow.cs
@@ -9,7 +9,7 @@ namespace Dialog.Abstractions.Wpf.Intefaces
         bool? DialogResult { get; set; }
         event EventHandler Closed;
         void Show();
-        //void Show(Grid gridContainer); //now implemented via extension method asdf
+        //void Show(Grid gridContainer); //now implemented via extension method in Xceed.Wpf.Toolkit.ChildWindowExtensions (Dialog.Implementors.Wpf\Extensions\ChildWindowExtensions.cs)
         object DataContext { get; set; }
         void Close();
     }
