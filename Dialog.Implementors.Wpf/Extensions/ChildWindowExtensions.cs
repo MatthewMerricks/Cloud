6b1549867cc7b043fdceac5f67b2f49bc19fa010
David Bruck <david.bruck@citrix.com>
2012-05-23 10:54:57 -0400

    Checking in required 3rdParty binaries along with changes to the projects so they are referenced at the new location, moved the Show method modification in the 3rdParty ChildWindow class to a new extension class outside the WPFToolkit library

---
+++ Dialog.Implementors.Wpf/Extensions/ChildWindowExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using Dialog.Abstractions.Wpf.Intefaces;
+using System.Windows;
+
+namespace Xceed.Wpf.Toolkit
+{
+    public static class ChildWindowExtensions
+    {
+        private static Dictionary<IModalWindow, Grid> childWindowContainers = new Dictionary<IModalWindow, Grid>();
+
+        public static void Show(this IModalWindow toExtend, Grid container)
+        {
+            UIElement extendElement = toExtend as UIElement;
+
+            if (extendElement != null)
+            {
+                lock (childWindowContainers)
+                {
+                    if (!childWindowContainers.ContainsKey(toExtend))
+                    {
+                        toExtend.Closed += toExtend_Closed;
+                        childWindowContainers.Add(toExtend, container);
+                        container.Children.Add(extendElement);
+                        container.ApplyTemplate();
+                    }
+                }
+                toExtend.Show();
+            }
+        }
+
+        private static void toExtend_Closed(object sender, EventArgs e)
+        {
+            IModalWindow castSender = (IModalWindow)sender;
+            UIElement senderElement = sender as UIElement;
+
+            if (senderElement != null)
+            {
+                lock (childWindowContainers)
+                {
+                    if (childWindowContainers.ContainsKey(castSender))
+                    {
+                        Grid container = childWindowContainers[castSender];
+                        if (container.Children.Contains(senderElement))
+                            container.Children.Remove(senderElement);
+                        childWindowContainers.Remove(castSender);
+                    }
+                }
+            }
+        }
+    }
+}
