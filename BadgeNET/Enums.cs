8b907a2d53f243a950fc14f42f46bea996ed21b9
David Bruck <david.bruck@citrix.com>
2012-06-05 09:53:11 -0400

    Converted BadgeType static string class wrapper to an enumeration matching FOFinderApiProtocol.h from OSX, cloudAppIconBadgeType

---
+++ BadgeNET/Enums.cs
@@ -0,0 +1,25 @@
+﻿//
+// BadgeType.cs
+// Cloud Windows
+//
+// Created By DavidBruck.
+// Copyright (c) Cloud.com. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadgeNET
+{
+    /// <summary>
+    /// Types of badges for icon overlays
+    /// </summary>
+    public enum BadgeType : byte
+    {
+        Synced = 1,
+        Syncing = 2,
+        Failed = 3,
+        Selective = 4
+    }
+}
