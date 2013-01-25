d93fd856a8e757c6454a91f67d20cb445a247e66
CloudBob <robert.stevens@citrix.com>
2013-01-25 10:28:53 -0500

    1) Add the UAC checks back in to the IsAdministrator() method.  Tested on all OSs.

--- CloudSdkSyncSample/Static/Helpers.cs
+++ CloudSdkSyncSample/Static/Helpers.cs
@@ -167,8 +167,60 @@ namespace CloudSdkSyncSample.Static
                 // If UAC is on, and the process is not elevated, then this will actually return false.
                 if (principal.IsInRole(WindowsBuiltInRole.Administrator))
                 {
-                    _trace.writeToLog(1, "Helpers: IsAdministrator: IsInRole adminstrator.  Return true.");
+                    _trace.writeToLog(9, "Helpers: IsAdministrator: IsInRole adminstrator.  Return true.");
                     return true;
+                }
+
+                // If we're not running in Vista onwards, we don't have to worry about checking for UAC.
+                if (Environment.OSVersion.Platform != PlatformID.Win32NT || Environment.OSVersion.Version.Major < 6)
+                {
+                    // Operating system does not support UAC; skipping elevation check.
+                    _trace.writeToLog(9, "Helpers: IsAdministrator: OS does not support UAC.  Return falsee.");
+                    return false;
+                }
+
+                int tokenInfLength = Marshal.SizeOf(typeof(int));
+                IntPtr tokenInformation = Marshal.AllocHGlobal(tokenInfLength);
+
+                try
+                {
+                    var token = identity.Token;
+                    var result = CloudSdkSyncSample.Static.NativeMethod.GetTokenInformation(token, CloudSdkSyncSample.Static.NativeMethod.TokenInformationClass.TokenElevationType, tokenInformation, tokenInfLength, out tokenInfLength);
+
+                    if (!result)
+                    {
+                        var exception = Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+                        throw new InvalidOperationException("Couldn't get token information", exception);
+                    }
+
+                    var elevationType = (CloudSdkSyncSample.Static.NativeMethod.TokenElevationType)Marshal.ReadInt32(tokenInformation);
+
+                    switch (elevationType)
+                    {
+                        case CloudSdkSyncSample.Static.NativeMethod.TokenElevationType.TokenElevationTypeDefault:
+                            // TokenElevationTypeDefault - User is not using a split token, so they cannot elevate.
+                            _trace.writeToLog(9, "Helpers: IsAdministrator: User is not using a split token, so they cannot elevate.  Return false.");
+                            return false;
+                        case CloudSdkSyncSample.Static.NativeMethod.TokenElevationType.TokenElevationTypeFull:
+                            // TokenElevationTypeFull - User has a split token, and the process is running elevated. Assuming they're an administrator.
+                            _trace.writeToLog(9, "Helpers: IsAdministrator: User has a split token, and the process is running elevated. Assuming they're an administrator. Return true.");
+                            return true;
+                        case CloudSdkSyncSample.Static.NativeMethod.TokenElevationType.TokenElevationTypeLimited:
+                            // TokenElevationTypeLimited - User has a split token, but the process is not running elevated. Assuming they're an administrator.
+                            _trace.writeToLog(9, "Helpers: IsAdministrator: IsInRole User has a split token, but the process is not running elevated. Return false.");
+                            return false;
+                        default:
+                            // Unknown token elevation type.
+                            _trace.writeToLog(9, "Helpers: IsAdministrator: Unknown token elevation type.  Return false.");
+                            return false;
+                    }
+                }
+                finally
+                {
+                    if (tokenInformation != IntPtr.Zero)
+                    {
+                        Marshal.FreeHGlobal(tokenInformation);
+                    }
                 }
             }
             catch (Exception ex)
@@ -176,9 +228,8 @@ namespace CloudSdkSyncSample.Static
                 CLError error = ex;
                 error.LogErrors(_trace.TraceLocation, _trace.LogErrors);
                 _trace.writeToLog(1, "Helpers: IsAdministrator: ERROR: Exception: Msg: <{0}>. Return false.", ex.Message);
+                return false;
             }
-
-            return false;
         }
     }
 }
