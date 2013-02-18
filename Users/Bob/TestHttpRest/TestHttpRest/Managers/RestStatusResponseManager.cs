3642dbc9863369290c96f70b7800ab0ec247fdc3
CloudBob <robert.stevens@citrix.com>
2013-02-18 08:32:56 -0500

    1) Added a private Users\Bob TestHttpRest project.  Work in progress.  It doesn't build yet.

---
+++ Users/Bob/TestHttpRest/TestHttpRest/Managers/RestStatusResponseManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudSDK_SmokeTest.Managers
+{
+    public sealed class RestStatusResponseManager
+    {
+        #region Properties
+        #endregion 
+
+        #region Init
+        #endregion 
+
+        #region Implementation 
+        #endregion 
+
+        #region Static
+        public static void HandleBadRequest(string opperationTarget)
+        {
+            if (opperationTarget == ManagerConstants.RequestTypes.PostFileChange)
+            { 
+            }
+            //TODO:Create logic for handling a bad request. 
+            Console.WriteLine("The Rest Reponse for Type {0} is Bad Request.", opperationTarget);
+        }
+
+        public static void HandleCancelled(string opperationTarget)
+        {
+            //TODO:Create logic for handling a cancelled request.
+            Console.WriteLine("The Rest Reponse for Type {0} is Cancelled.", opperationTarget);
+        }
+
+        public static void HandleConnectionFailed(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where the connection failed.
+            Console.WriteLine("The Rest Reponse for Type {0} is Connection Failed.", opperationTarget);
+        }
+
+        public static void HandleNoContent(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where file has no content. 
+            Console.WriteLine("The Rest Reponse for Type {0} is No Content.", opperationTarget);
+        }
+
+        public static void HandleNotAutorized(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where the operation is not authorized.
+            Console.WriteLine("The Rest Reponse for Type {0} is Not Authorized.", opperationTarget);
+        }
+
+        public static void HandleNotFound(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where the file is not found. 
+            Console.WriteLine("The Rest Reponse for Type {0} is Not Found.", opperationTarget);
+        }
+
+        public static void HandleQuotaExceeded(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where the syncbox Quota is exceeded.
+            Console.WriteLine("The Rest Reponse for Type {0} is Quota Exceeded.", opperationTarget);
+        }
+
+        public static void HandleServerError(string opperationTarget)
+        {
+            //TODO:Create logic for handling a request where server returns a Server errror. 
+            Console.WriteLine("The Rest Reponse for Type {0} is Server Error.", opperationTarget);
+        }
+        #endregion 
+
+        #region Private
+        #endregion
+    }
+}
---
+++ Users/Bob/TestHttpRest/TestHttpRest/Settings/AdvancedSyncSettings.cs
@@ -0,0 +1,205 @@
+﻿using CloudApiPublic.Interfaces;
+using CloudApiPublic.Static;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudSDK_SmokeTest.Settings
+{
+    internal sealed class AdvancedSyncSettings : ICLSyncSettingsAdvanced
+    {
+        /// <summary>
+        /// Set to true if badging should be enabled
+        /// </summary>
+        public bool BadgingEnabled
+        {
+            get
+            {
+                return _badgingEnabled;
+            }
+        }
+        private readonly bool _badgingEnabled = false;
+
+        /// <summary>
+        /// Set to true if errors should be logged.
+        /// </summary>
+        public bool LogErrors
+        {
+            get
+            {
+                return _logErrors;
+            }
+        }
+        private readonly bool _logErrors = false;
+
+        /// <summary>
+        /// Determines the information that is traced.
+        /// </summary>
+        public TraceType TraceType
+        {
+            get
+            {
+                return _traceType;
+            }
+        }
+        private readonly TraceType _traceType = TraceType.NotEnabled;
+
+        /// <summary>
+        /// Only required if TraceType has any flags set (TraceType.NotEnabled means no flags are set).
+        /// Provides the path to the directory that will hold the trace files.
+        /// </summary>
+        public string TraceLocation
+        {
+            get
+            {
+                return _traceLocation;
+            }
+        }
+        private readonly string _traceLocation;
+
+        /// <summary>
+        /// Set to true to exclude authorization information from the trace.
+        /// </summary>
+        public bool TraceExcludeAuthorization
+        {
+            get
+            {
+                return _traceExcludeAuthorization;
+            }
+        }
+        private readonly bool _traceExcludeAuthorization;
+
+        /// <summary>
+        /// Specify 1 for the only the most important traces.  Use a higher number for more detail.
+        /// </summary>
+        public int TraceLevel
+        {
+            get
+            {
+                return _traceLevel;
+            }
+        }
+        private readonly int _traceLevel;
+
+        public string DeviceId
+        {
+            get
+            {
+                return _deviceId;
+            }
+        }
+        private readonly string _deviceId;
+
+        /// <summary>
+        /// If null, a precalculated value will be used based on the local, non-roaming user's application data in the Cloud subdirectory
+        /// </summary>
+        public string TempDownloadFolderFullPath
+        {
+            get
+            {
+                return _tempDownloadFolderFullPath;
+            }
+        }
+        private readonly string _tempDownloadFolderFullPath = null;
+
+        public string ClientVersion
+        {
+            get
+            {
+                return _clientVersion;
+            }
+        }
+        private readonly string _clientVersion = null;
+
+        public string FriendlyName
+        {
+            get
+            {
+                return _friendlyName;
+            }
+        }
+        private readonly string _friendlyName = null;
+
+        public string SyncRoot
+        {
+            get
+            {
+                return _syncRoot;
+            }
+        }
+        private readonly string _syncRoot = null;
+
+        public string DatabaseFolder
+        {
+            get
+            {
+                return _databaseFolder;
+            }
+        }
+        private readonly string _databaseFolder = null;
+
+        public static AdvancedSyncSettings CreateDefaultSettings()
+        {
+            return new AdvancedSyncSettings(
+                false,
+                TraceType.NotEnabled,
+                null,
+                true,
+                0,
+                Environment.MachineName + Guid.NewGuid().ToString("N"),
+                null,
+                "SimpleClient01",
+                Environment.MachineName,
+                null,
+                null);
+        }
+
+        public static AdvancedSyncSettings CreateDefaultSettings(ICLSyncSettings syncSettings)
+        {
+            if (syncSettings == null)
+            {
+                throw new ArgumentNullException("syncSettings must not be null");
+            }
+
+            return new AdvancedSyncSettings(
+                false,
+                TraceType.NotEnabled,
+                null,
+                true,
+                0,
+                Environment.MachineName + Guid.NewGuid().ToString("N"),
+                null,
+                "SimpleClient01",
+                Environment.MachineName,
+                syncSettings.SyncRoot,
+                null);
+        }
+
+        public AdvancedSyncSettings(
+                    bool logErrors,
+                    TraceType traceType,
+                    string traceLocation,
+                    bool traceExcludeAuthorization,
+                    int traceLevel,
+                    string deviceId,
+                    string tempDownloadFolderFullPath,
+                    string clientVersion,
+                    string friendlyName,
+                    string cloudRoot,
+                    string databaseFolder)
+        {
+            this._logErrors = logErrors;
+            this._traceType = traceType;
+            this._traceLocation = traceLocation;
+            this._traceExcludeAuthorization = traceExcludeAuthorization;
+            this._traceLevel = traceLevel;
+            this._deviceId = deviceId;
+            this._tempDownloadFolderFullPath = tempDownloadFolderFullPath;
+            this._clientVersion = clientVersion;
+            this._friendlyName = friendlyName;
+            this._syncRoot = cloudRoot;
+            this._databaseFolder = databaseFolder;
+        }
+    }
+}
