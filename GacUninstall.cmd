b0431b223644b194d4c9b59f171695404f4de954
Bob Stevens <robert.stevens@citrix.com>
2013-03-05 19:21:12 -0500

    1) Removed "require specific version" from the Cloud reference in the sample app project.
    2) Added a command file and a new project driver "ZGacUninstall" to uninstall our files from the GAC.

--- GacUninstall.cmd
+++ GacUninstall.cmd
@@ -1,39 +1,57 @@
-echo gacutil /u BadgeCOMLib,Version=0.1.6.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u BadgeCOMLib,Version=0.1.6.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u BadgeCOMLib,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u BadgeCOMLib,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u Cloud,Version=0.1.5.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u Cloud,Version=0.1.5.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u ErikEJ.SqlCe40.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u ErikEJ.SqlCe40.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u ErikEJ.SqlCe40,Version=2.1.6.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u ErikEJ.SqlCe40,Version=2.1.6.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u ErikEJ.SqlCe40Cloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u ErikEJ.SqlCe40Cloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u Microsoft.CSharp,Version=4.0.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
-gacutil /u Microsoft.CSharp,Version=4.0.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
+echo gacutil /u Microsoft.Net.Http.CSDK,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
+gacutil /u Microsoft.Net.Http.CSDK,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
 
-echo gacutil /u Microsoft.Net.Http,Version=0.3.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
-gacutil /u Microsoft.Net.Http,Version=0.3.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
+echo gacutil /u Microsoft.Net.HttpCloud,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
+gacutil /u Microsoft.Net.HttpCloud,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
 
-echo gacutil /u Microsoft.Practices.ServiceLocation,Version=1.0.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u Microsoft.Practices.ServiceLocation,Version=1.0.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u Microsoft.Practices.ServiceLocation.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Microsoft.Practices.ServiceLocation.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u Newtonsoft.Json,Version=4.5.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u Newtonsoft.Json,Version=4.5.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u Microsoft.Practices.ServiceLocationCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Microsoft.Practices.ServiceLocationCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u RateBar,Version=1.0.4762.20666,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u RateBar,Version=1.0.4762.20666,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u Newtonsoft.Json.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Newtonsoft.Json.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u Salient.Data,Version=1.0.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u Salient.Data,Version=1.0.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u Newtonsoft.JsonCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Newtonsoft.JsonCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u SimpleJson,Version=0.1.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u SimpleJson,Version=0.1.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u RateBar.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u RateBar.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u SuperSocket.ClientEngine.Core,Version=0.2.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u SuperSocket.ClientEngine.Core,Version=0.2.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u RateBarCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u RateBarCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u SuperSocket.ClientEngine.Protocol,Version=0.2.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
-gacutil /u SuperSocket.ClientEngine.Protocol,Version=0.2.0.0,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+echo gacutil /u Salient.Data.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Salient.Data.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
-echo gacutil /u System.Dynamic,Version=4.0.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
-gacutil /u System.Dynamic,Version=4.0.0.0,Culture=neutral,PublicKeyToken=b03f5f7f11d50a3a
+echo gacutil /u Salient.DataCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Salient.DataCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u SimpleJson.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u SimpleJson.CSDK,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u SimpleJsonCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u SimpleJsonCloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u SuperSocket.ClientEngine.Core,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u SuperSocket.ClientEngine.Core,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u Cloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u Cloud,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u SuperSocket.ClientEngine.Common,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u SuperSocket.ClientEngine.Common,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+
+echo gacutil /u SuperSocket.ClientEngine.Protocol,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
+gacutil /u SuperSocket.ClientEngine.Protocol,Culture=neutral,PublicKeyToken=840eb90fa4503c3c
 
