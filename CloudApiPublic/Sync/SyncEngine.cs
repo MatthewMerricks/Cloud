ee1000c468cd048cb839e938f331ae4ff87e1a57
Bob Stevens <robert.stevens@citrix.com>
2013-05-10 12:17:28 -0400

    1) Added original code to set fileChangeForOriginalMetadata in the case where we have a matched changee.

--- CloudApiPublic/Sync/SyncEngine.cs
+++ CloudApiPublic/Sync/SyncEngine.cs
@@ -8830,6 +8830,35 @@ namespace Cloud.Sync
                                                     };
                                                 }
                                             }
+                                            // else if a previous matched change for the current event was found, then set the FileChange to use for previous metadata from the matched change
+                                            //&&&& RKS Added.  Check this.
+                                            else
+                                            {
+                                                fileChangeForOriginalMetadata = ((PossiblyStreamableFileChange)matchedChange).FileChange;
+
+                                                // puts back the latest updated revision from the server (which would have been lost when the metadata instance would be replaced next below), update revision changer for difference
+                                                //&&&& Re-added and updated. RKS Save the revision to the DB here.  What about the serverUid?
+                                                //&&&& old code
+                                                //if (fileChangeForOriginalMetadata.Metadata != null
+                                                //    && string.IsNullOrEmpty(fileChangeForOriginalMetadata.Metadata.Revision)
+                                                //    && currentChange.Metadata != null
+                                                //    && !string.IsNullOrEmpty(currentChange.Metadata.Revision))
+                                                //{
+                                                //    fileChangeForOriginalMetadata.Metadata.Revision = currentChange.Metadata.Revision;
+
+                                                //    fileChangeForOriginalMetadata.Metadata.RevisionChanger.FireRevisionChanged(fileChangeForOriginalMetadata.Metadata);
+                                                //}
+                                                //&&&& end old code
+                                                //&&&& new code
+                                                if (fileChangeForOriginalMetadata.Metadata != null
+                                                    && string.IsNullOrEmpty(ReturnAndPossiblyFillUidAndRevision(uidStorage, syncData, fileChangeForOriginalMetadata.Metadata.ServerUidId).Revision)
+                                                    && currentChange.Metadata != null
+                                                    && !string.IsNullOrEmpty(ReturnAndPossiblyFillUidAndRevision(uidStorage, syncData, currentChange.Metadata.ServerUidId).Revision))
+                                                {
+                                                    fileChangeForOriginalMetadata.Metadata = fileChangeForOriginalMetadata.Metadata.CopyWithNewServerUidId(currentChange.Metadata.ServerUidId);
+                                                }
+                                                //&&&& end new code
+                                            }
 
                                             // set the metadata of the current FileChange as the metadata from the previous change (or fake previous change if server was queried for new metadata)
                                             currentChange.Metadata = fileChangeForOriginalMetadata.Metadata;
