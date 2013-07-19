459c6d8cab1b2444ccc993876607ab2e3a6eaae8
David Bruck <david.bruck@citrix.com>
2013-07-19 15:45:16 -0400

    -part 2 of 2-
    1) serializing/deserializing credentials json contracts are currently in a broken state! need to figure out how to properly dual-purpose json fields for single items versus collections
    2) removed Newtonsoft.Json 3rd-party library since we already have SimpleJson; where it was used in CLCredentials still needs to be fixed to use DataContractJsonSerializer correctly

--- CloudApiPublic/JsonContracts/CredentialsSessionsResponse.cs
+++ CloudApiPublic/JsonContracts/CredentialsSessionsResponse.cs
@@ -1,5 +1,5 @@
 ﻿//
-// CredentialsSessionGetForKeyResponse.cs
+// CredentialsSessionsResponse.cs
 // Cloud Windows
 //
 // Created By BobS.
@@ -17,19 +17,123 @@ using System.Text;
 namespace Cloud.JsonContracts
 {
     /// <summary>
-    /// Result from <see cref="Cloud.CLCredentials.ShowSession"/>
+    /// Result from <see cref="Cloud.CLCredentials.SessionCredentialsForKey"/> and <see cref="Cloud.CLCredentials.ListAllActiveSessionCredentials"/>
     /// </summary>
     [Obfuscation(Exclude = true)]
-    [DataContract]
-    public sealed class CredentialsSessionGetForKeyResponse
+    [Serializable] // -David: converted to ISerializable to prevent Newtonsoft.Json manual parsing
+    [KnownType(typeof(Session[]))]
+    [KnownType(typeof(Session))]
+    public sealed class CredentialsSessionsResponse : ISerializable  // -David: converted to ISerializable to prevent Newtonsoft.Json manual parsing
     {
-        [DataMember(Name = CLDefinitions.RESTResponseStatus, IsRequired = false)]
         public string Status { get; set; }
 
-        [DataMember(Name = CLDefinitions.RESTResponseMessage, IsRequired = false)]
         public string Message { get; set; }
 
-        [DataMember(Name = CLDefinitions.RESTResponseSession, IsRequired = false)]
-        public Session Session { get; set; }
+        public Session[] Sessions { get; set; }
+
+        #region ISerializable members -David: converted to ISerializable to prevent Newtonsoft.Json manual parsing
+
+        // why does this work?? (called upon ReadObject on a JsonDataContractSerializer on a response stream)
+        protected CredentialsSessionsResponse(SerializationInfo info, StreamingContext context)
+        {
+            foreach (SerializationEntry toAdd in info)
+            {
+                switch (toAdd.Name)
+                {
+                    case CLDefinitions.RESTResponseStatus:
+                        if (toAdd.ObjectType == typeof(string))
+                        {
+                            this.Status = (string)toAdd.Value;
+                        }
+                        else
+                        {
+                            throw new CLInvalidOperationException(
+                                CLExceptionCode.General_Arguments,
+                                string.Format(
+                                    Resources.ExceptionSerializationInfoInvalidObjectType,
+                                    CLDefinitions.RESTResponseStatus,
+                                    typeof(string),
+                                    toAdd.ObjectType));
+                        }
+                        break;
+
+                    case CLDefinitions.RESTResponseMessage:
+                        if (toAdd.ObjectType == typeof(string))
+                        {
+                            this.Message = (string)toAdd.Value;
+                        }
+                        else
+                        {
+                            throw new CLInvalidOperationException(
+                                CLExceptionCode.General_Arguments,
+                                string.Format(
+                                    Resources.ExceptionSerializationInfoInvalidObjectType,
+                                    CLDefinitions.RESTResponseMessage,
+                                    typeof(string),
+                                    toAdd.ObjectType));
+                        }
+                        break;
+
+                    case CLDefinitions.RESTResponseSession:
+                        //if (toAdd.ObjectType == typeof(string))
+                        //{
+                        //    this.ServiceType = (string)toAdd.Value;
+                        //}
+                        //else
+                        //{
+                        throw new CLInvalidOperationException(
+                            CLExceptionCode.General_Arguments,
+                            string.Format(
+                                Resources.ExceptionSerializationInfoInvalidObjectType,
+                                CLDefinitions.RESTResponseSession,
+                                typeof(string),
+                                toAdd.ObjectType));
+                    //}
+                    //break;
+
+                    case CLDefinitions.RESTResponseSession_Sessions:
+                        //if (toAdd.ObjectType == typeof(string))
+                        //{
+                        //    this.ServiceType = (string)toAdd.Value;
+                        //}
+                        //else
+                        //{
+                        throw new CLInvalidOperationException(
+                            CLExceptionCode.General_Arguments,
+                            string.Format(
+                                Resources.ExceptionSerializationInfoInvalidObjectType,
+                                CLDefinitions.RESTResponseSession_Sessions,
+                                typeof(string),
+                                toAdd.ObjectType));
+                    //}
+                    //break;
+                }
+            }
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (this.Status != null)
+            {
+                info.AddValue(CLDefinitions.RESTResponseStatus, this.Status);
+            }
+
+            if (this.Message != null)
+            {
+                info.AddValue(CLDefinitions.RESTResponseMessage, this.Message);
+            }
+
+            if (this.Sessions != null)
+            {
+                if (this.Sessions.Length == 1)
+                {
+                    info.AddValue(CLDefinitions.RESTResponseSession, this.Sessions[0]);
+                }
+
+                info.AddValue(CLDefinitions.RESTResponseSession_Sessions, this.Sessions);
+            }
+        }
+
+        #endregion
     }
 }
